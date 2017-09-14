05f258d67a31b8e83e99294bd861dfb810d85ac0
maxencej <maxence.joncheray@gmail.com>
2017-09-14 11:39:53 +0200

    update for tp1

--- app.js
+++ app.js
@@ -16,25 +16,72 @@ var connector = new botbuilder.ChatConnector({
 // Listening for user inputs
 server.post('/api/messages', connector.listen());
 
+
+var savedAddress;
 // Reply by echoing
 var bot = new botbuilder.UniversalBot(connector, function(session){
+    savedAddress = session.message.address;
     session.send('You say : %s | [length: %s]', session.message.text, session.message.text.length);
-    bot.on('typing', function () {
-        session.send('Tu tapes !');
-    });
-
-    bot.on('conversationUpdate', function(message) {
-        if (message.membersAdded && message.membersAdded.length > 0) {
-            var membersAdded = message.membersAdded.map(function (x) {
-                var isSelf = x.id == message.address.bot.id;
-                return (isSelf ? message.address.bot.name : x.name) || ' ' + '(Id=' + x.id + ' )'
-            }).join(', ');
-            bot.send(new botbuilder.Message()
-                .address(message.address)
-                .text('Bienvenue ' + membersAdded));
-        }
-    });
 });
+
+bot.on('typing', function (response) {
+    session.send(response);
+    session.send('Tu tapes !');
+});
+
+bot.dialog('/first', function (session) {
+    botbuilder.Prompts.text(session, 'Bien le bonjour !')
+});
+
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.address.bot.name;
+        var id = message.address.bot.id;
+        var reply = new botbuilder.Message()
+            .address(message.address)
+            .text("Bienvenue à name : %s | id : %s", name, id);
+        bot.send(reply);
+    } else if(message.action === 'remove'){
+        var botId = message.address.bot.id;
+        var reply = new botbuilder.Message()
+            .address(message.address)
+            .text('L\'utilisateur : ' + botId + ' vient de nous quitter');
+        bot.send(reply);
+    }
+});
+
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        var membersAdded = message.membersAdded
+            .map(function (m) {
+                var isSelf = m.id === message.address.bot.id;
+                if (isSelf) {
+                    return message.address.bot.name;
+                } else {
+                    return 'name : ' + m.name + ' | id : ' + m.id;
+                }
+            })
+            .join(', ');
+
+        bot.send(new botbuilder.Message()
+            .address(message.address)
+            .text('Bienvenue à ' + membersAdded));
+    }
+
+    if (message.membersRemoved && message.membersRemoved.length > 0) {
+        var membersRemoved = message.membersRemoved
+            .map(function (m) {
+                var isSelf = m.id === message.address.bot.id;
+                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
+            })
+            .join(', ');
+
+        bot.send(new botbuilder.Message()
+            .address(message.address)
+            .text('L\'utilisateur : ' + membersRemoved + ' vient de nous quitter'));
+    }
+});
+
 
 // bot doit capter que le user ecrit
 // ajout user bot doit renvoyer les informations du user nom / id
